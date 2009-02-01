794ae5b516b221b6a3658b8e8db19a0d3e59f3aa
Jacob Stetser <jake@wideopenspac.es>
2009-02-01 16:30:35 -0700

    Removed 'notnot' and replaced with 'andnot' which fits better as opposite of 'andand'. Reorganized and clarified inline docs, moved some methods around. Added versions of aint_nothin? and is_nothin? with 'nothing' spelled correctly.

--- lib/wicked.rb
+++ lib/wicked.rb
@@ -7,63 +7,77 @@ module Wicked
   # Got it? Wicked Good!
   module Good
     
-    # Aliases for 'andand'
-    #     if phone = user.et.number; .... ; end
+    # Alias for 'andand' that reads like 'if user and number' 
+    #     if user.et.number...
     alias_method :et, :andand       # Latin for 'and'
 
-    #     if phone = user.ask.number ....
+    # Alias for 'andand' that reads like 'if user, ask its number' 
+    #     if user.ask.number...
     alias_method :ask, :andand
     
     # Alias for andand that's meaningful when checking booleans; 
     # this is the positive version of aint.
     #     "don't fix it" unless it.is.broken?
-    # Returns nil if 'it' is broken, so this is safer than:
-    #     "don't fix it" unless it.broken?
-    alias_method :is, :andand       
+    alias_method :is, :andand  
+    
+    # :nodoc:
+    alias_method :aintnot, :andand   
     
     # Guarded method negation in the style of andand
     # Returns the logical opposite of the method invoked on the receiver
-    #    receiver.aint.method
-    # Traps NoMethodError and returns nil if receiver is nil 
-    # or the method does not exist on the receiver. 
+    # Returns nil if receiver is nil or doesn't have method. 
     #
-    #     [].aint.empty?
-    #     => false
-    #     [].aint.nil?
-    #     => true
-    #     it.aint.broken?
-    #     => false
+    #     [].aint.empty?  => false
+    #     [].aint.nil?    => true
+    #     it.aint.broken? => false
     #
-    # Don't be a doofus, use this wisely, on boolean methods only
-    # if you know what's good for you.
-
+    # Don't be a doofus, use this wisely, on boolean methods only if you know what's good for you.
     def aint(p = nil)
       NotReturningMe.new(self) 
     end
     
     # A funny byproduct of using aint...
-    # But before you get confused, read it as 'is not nothing, which should be true except for nil)
+    # nil.aint_nothin? => false
     def aint_nothin?
       self.aint.nil?
     end
-    # See? You can laugh...
     alias_method :aint_it_somethin?, :aint_nothin?
     
-    # Ok, so here goes - true if nothing, like nil
+    # nil.is_nothin? => true
     def is_nothin?
       !self.is.nil?
     end
     
-    # A few stylistic variations on aint
+    # A few stylistic variations on aint for the grammatically uncreative ;)
+    # --   
+    # 'andnot' is probably the closest analogue to 'andand'  (and Ruby's 
+    # syntax - && !), unless you want to write out 'andandnot' :)
+    alias_method :andnot, :aint
+    
     alias_method :isnt, :aint
     alias_method :is_not, :aint
-    alias_method :notnot, :aint
+    # Removed #notnot in favor of #andnot
     
-    # Hehe
-    alias_method :aintnot, :andand
-    # As in 'User, eh? Wot's yer number?'
-    #     if phone = user.eh?.number; .... ; end
-    alias_method :eh?, :andand
+    # Just in case you can't keep your fingers off the Gs
+    alias_method :aint_nothing?, :aint_nothin?
+    alias_method :is_nothing?, :is_nothin?
+    
+    
+    
+    # Are you reading my notes? Well, cool!
+    # I wrote this for my personal site -- jacob-stetser.com -- and thought
+    # it might be useful to other people, so I extracted it!
+    
+    # Here's a few real-world uses:
+    # [Create or find a user, assign to 'user' and check if new record all in one line]
+    #    if (user = User.find_or_initialize_by_email(self.author_email)).is.new_record? ... end
+    # 
+    # [Safely simplify chained expressions] from:
+    #    if @current_user and role = @current_user.role and role.admin?
+    # to:
+    #    if @current_user.ask.role.is.admin? 
+    #
+    # Wicked Sweet. Ciao!
   end
   
 end
